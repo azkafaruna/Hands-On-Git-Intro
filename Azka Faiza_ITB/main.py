c3256e64831f49b4d11b388defa857203a973206
azkafaruna <118370773+azkafaruna@users.noreply.github.com>
2022-11-16 22:35:14 +0700

    Add files via upload

---
+++ Azka Faiza_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: Azka Faiza Aruna")
+print("Jurusan: TPB FTMD")
+print("Universitas: Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC: Ingin belajar tentang pemrograman di luar mata kuliah dan berharap bisa lebih mendalaminya")
